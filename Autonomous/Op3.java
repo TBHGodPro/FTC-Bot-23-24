0a58a99a5585d0d776b2c9b16a85b58c767778ae
TBHGodPro <tarun.bhaskar@outlook.com>
2023-11-04 22:18:52 -0500

    4 basic auto ops

---
+++ Autonomous/Op3.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.AutoOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Movements.*;
+import org.firstinspires.ftc.teamcode.Movements.Actions.*;
+
+@Autonomous(name = "Auto Op 3")
+public class Op3 extends AutoOp {
+    public static final Action[] movements = {
+            // ACTIONS HERE
+
+            new MoveAction(MoveDirection.FORWARD, 900, 0.5),
+            new MoveAction(MoveDirection.LEFT, 500, 0.5),
+            new MoveAction(MoveDirection.BACKWARD, 900, 0.5),
+            new TurnAction(70, 1),
+            new GamepadAction(GamepadButton.b),
+            new WaitAction(100),
+            new GamepadAction(GamepadButton.y),
+            new GamepadAction(GamepadDynamicInput.right_trigger, 0.5f),
+            new WaitAction(2000),
+            new GamepadAction(GamepadDynamicInput.right_trigger, 0f),
+
+            // ACTIONS HERE
+    };
+
+    public Op3() {
+        super(movements);
+    }
+}
