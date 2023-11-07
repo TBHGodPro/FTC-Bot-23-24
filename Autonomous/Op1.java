10ff842596d83726c4cb9bf3764b5b6fe7e82040
TBHGodPro <tarun.bhaskar@outlook.com>
2023-11-07 14:27:23 -0600

    Prep New Auton

--- Autonomous/Op1.java
+++ Autonomous/Op1.java
@@ -4,29 +4,14 @@ import org.firstinspires.ftc.teamcode.AutoOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.Movements.*;
-import org.firstinspires.ftc.teamcode.Movements.Actions.*;
-
 @Autonomous(name = "Auto Op 1")
 public class Op1 extends AutoOp {
-    public static final Action[] movements = {
-            // ACTIONS HERE
-
-            new MoveAction(MoveDirection.FORWARD, 900, 0.5),
-            new MoveAction(MoveDirection.LEFT, 500, 0.5),
-            new MoveAction(MoveDirection.BACKWARD, 900, 0.5),
-            new TurnAction(70, 1),
-            new GamepadAction(GamepadButton.b),
-            new WaitAction(100),
-            new GamepadAction(GamepadButton.y),
-            new GamepadAction(GamepadDynamicInput.right_trigger, 0.5f),
-            new WaitAction(2000),
-            new GamepadAction(GamepadDynamicInput.right_trigger, 0f),
-
-            // ACTIONS HERE
-    };
-
-    public Op1() {
-        super(movements);
+    public void prepTFOD() {
+        tfod.TFOD_Model = "/sdcard/FIRST/tflitemodels/CenterStage.tflite";
+        tfod.Labels.add("Pixel");
+    }
+    
+    public void runLoop() {
+        gamepad.right_trigger = 0.25f;
     }
 }
