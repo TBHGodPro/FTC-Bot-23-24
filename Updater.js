e96becd8c3ae6be98e826cd9877170501ce56833
TBHGodPro <tarun.bhaskar@outlook.com>
2023-11-09 13:45:47 -0600

    Fixed updater

--- Updater.js
+++ Updater.js
@@ -5,15 +5,14 @@ const axios = require('axios');
 fs.watch(
   resolve(__dirname, 'org/firstinspires/ftc/teamcode'),
   {
-    persistent: true,
     recursive: true,
   },
-  (type, file) => {
+  async (type, file) => {
     switch (type) {
       case 'change': {
         console.log(`Change detected in "${file}"`);
 
-        axios
+        const res = await axios
           .post(
             `http://192.168.43.1:8080/java/file/save?f=/src/org/firstinspires/ftc/teamcode/${file}`,
             {
@@ -23,16 +22,23 @@ fs.watch(
               headers: {
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
               },
+              timeout: 1250,
             }
           )
-          .then(res => {
-            if (res.status == 200) {
-              console.log('Saved!');
-            } else {
-              console.error(res);
-              process.exit(1);
-            }
-          });
+          .then(res => res)
+          .catch(err => (err.code || !err.response ? err : err.response));
+
+        if (res instanceof Error) {
+          if (res.code === 'ECONNABORTED') console.error('Not Connected!');
+          else throw res;
+        } else {
+          if (res.status == 200) {
+            console.log('Saved!');
+          } else {
+            console.error(res);
+            process.exit(1);
+          }
+        }
 
         break;
       }
