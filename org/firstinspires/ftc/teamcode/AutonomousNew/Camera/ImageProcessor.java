9c5eed83c727f4b5f641b9a5c5a36ceaabd5dda9
TBHGodPro <tarun.bhaskar@outlook.com>
2023-11-09 20:43:09 -0600

    3 Image Regions

--- org/firstinspires/ftc/teamcode/AutonomousNew/Camera/ImageProcessor.java
+++ org/firstinspires/ftc/teamcode/AutonomousNew/Camera/ImageProcessor.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.AutonomousNew.Camera;
 import android.graphics.Canvas;
 
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.teamcode.AutonomousNew.BaseOp;
 import org.firstinspires.ftc.vision.VisionProcessor;
 
 import org.opencv.core.Mat;
@@ -11,24 +12,67 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.core.Scalar;
 
 public class ImageProcessor implements VisionProcessor {
+    static final Scalar BLUE = new Scalar(0, 0, 255);
+    static final Scalar GREEN = new Scalar(0, 255, 0);
+
+    static final int SCREEN_WIDTH = 640;
+    static final int SCREEN_HEIGHT = 480;
+
+    static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(0, 0);
+    static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(SCREEN_WIDTH / 3, 0);
+    static final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(SCREEN_WIDTH / 1.5, 0);
+
+    static final int REGION_WIDTH = SCREEN_WIDTH / 3;
+    static final int REGION_HEIGHT = SCREEN_HEIGHT;
+
+    Point region1_pointA = new Point(
+            REGION1_TOPLEFT_ANCHOR_POINT.x,
+            REGION1_TOPLEFT_ANCHOR_POINT.y);
+    Point region1_pointB = new Point(
+            REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+    Point region2_pointA = new Point(
+            REGION2_TOPLEFT_ANCHOR_POINT.x,
+            REGION2_TOPLEFT_ANCHOR_POINT.y);
+    Point region2_pointB = new Point(
+            REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+    Point region3_pointA = new Point(
+            REGION3_TOPLEFT_ANCHOR_POINT.x,
+            REGION3_TOPLEFT_ANCHOR_POINT.y);
+    Point region3_pointB = new Point(
+            REGION3_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            REGION3_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
         // Code executed on the first frame dispatched into this VisionProcessor
     }
 
     @Override
-    public Object processFrame(Mat frame, long captureTimeNanos) {
+    public Object processFrame(Mat input, long captureTimeNanos) {
         // Actual computer vision magic will happen here
 
         Imgproc.rectangle(
-                frame,
-                new Point(
-                        frame.cols() / 4,
-                        frame.rows() / 4),
-                new Point(
-                        frame.cols() * (3f / 4f),
-                        frame.rows() * (3f / 4f)),
-                new Scalar(0, 255, 0), 4);
+                input, // Buffer to draw on
+                region1_pointA, // First point which defines the rectangle
+                region1_pointB, // Second point which defines the rectangle
+                BLUE, // The color the rectangle is drawn in
+                2); // Thickness of the rectangle lines
+
+        Imgproc.rectangle(
+                input, // Buffer to draw on
+                region2_pointA, // First point which defines the rectangle
+                region2_pointB, // Second point which defines the rectangle
+                BLUE, // The color the rectangle is drawn in
+                2); // Thickness of the rectangle lines
+
+        Imgproc.rectangle(
+                input, // Buffer to draw on
+                region3_pointA, // First point which defines the rectangle
+                region3_pointB, // Second point which defines the rectangle
+                BLUE, // The color the rectangle is drawn in
+                2); // Thickness of the rectangle lines
 
         return null; // No context object
     }
