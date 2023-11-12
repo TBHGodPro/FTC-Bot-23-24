9cbf9ad73562760a886344a9fbe2600af5019c15
TBHGodPro <tarun.bhaskar@outlook.com>
2023-11-11 18:19:39 -0600

    Alliance-Specific Prop Basics

--- org/firstinspires/ftc/teamcode/AutonomousNew/Camera/OpenCVManager.java
+++ org/firstinspires/ftc/teamcode/AutonomousNew/Camera/OpenCVManager.java
@@ -4,15 +4,20 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.AutonomousNew.BaseOp;
 import org.firstinspires.ftc.teamcode.AutonomousNew.Camera.ImageProcessor;
+import org.firstinspires.ftc.teamcode.AutonomousNew.Util.*;
 
 public class OpenCVManager {
+    public Alliance alliance;
+
     public VisionPortal portal;
 
     public ImageProcessor processor;
 
     public WebcamName camera;
 
-    public OpenCVManager(WebcamName camera) {
+    public OpenCVManager(Alliance alliance, WebcamName camera) {
+        this.alliance = alliance;
+
         this.camera = camera;
 
         processor = new ImageProcessor();
